d1c349a033a8b9d5cca2095c6533404ae2e52877
William Aristea Tantiono <waristea@gmail.com>
2019-04-24 21:48:08 +0700

    Change facial-recognition to simple arial human-recognition

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 import numpy as np
 import cv2
 
@@ -30,3 +31,32 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+"""
+# https://medium.com/@madhawavidanapathirana/real-time-human-detection-in-computer-vision-part-2-c7eda27115c6
+import cv2
+import time
+import os
+
+person_cascade = cv2.CascadeClassifier(os.path.join('opencv/data/haarcascades/haarcascade_fullbody.xml'))
+cap = cv2.VideoCapture("TownCentre.mp4")
+
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
+
+while True:
+    r, frame = cap.read()
+    if r:
+        start_time = time.time()
+        frame = cv2.resize(frame,(640,360)) # Downscale to improve frame rate
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) # Haar-cascade classifier needs a grayscale image
+        rects = person_cascade.detectMultiScale(gray_frame)
+
+        end_time = time.time()
+        print("Elapsed Time:",end_time-start_time)
+
+        for (x, y, w, h) in rects:
+            cv2.rectangle(frame, (x,y), (x+w,y+h),(0,255,0),2)
+        cv2.imshow("preview", frame)
+    k = cv2.waitKey(1)
+    if k & 0xFF == ord("q"): # Exit condition
+        break
